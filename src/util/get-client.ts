f7627e7479dfbc8c8f904b332d62f703191b255e
Jeffrey Chupp <jeff@semanticart.com>
2023-11-15 11:27:37 -0500

    Export version
    The package.json isn't available in the installed context

--- src/util/get-client.ts
+++ src/util/get-client.ts
@@ -1,15 +1,11 @@
-import * as fs from 'node:fs'
-import path from 'node:path'
-
 import {Client} from '../prefab-common/src/api/client.js'
+import version from '../version.js'
 import {log} from './log.js'
 
 let clientInstance: Client | undefined
 
 const getClient = () => {
   if (clientInstance) return clientInstance
-
-  const {version} = JSON.parse(fs.readFileSync(path.join(path.dirname(''), './package.json'), 'utf8'))
 
   clientInstance = new Client({
     apiKey: process.env.PREFAB_API_KEY,
---
+++ src/version.ts
@@ -0,0 +1,2 @@
+// NOTE: This file is generated
+export default version = "0.0.3"
